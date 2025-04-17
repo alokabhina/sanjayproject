12ebba8695b849f7300a35b70101bddd7a99c3ca
alokabhinandan <alokabhinandan123@gmail.com>
2025-04-17 14:11:14 +0530

    Fix import path for header.css

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './header.css';
+import './Header.css';
 
 const Header = ({ onMenuToggle }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
